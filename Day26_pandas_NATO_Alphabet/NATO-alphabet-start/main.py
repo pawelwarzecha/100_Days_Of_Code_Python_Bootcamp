c6026ecb904f1e6197e122cffd76f0ee3e31cda2
pawelwarzecha <pawelwarzecha99@gmail.com>
2025-01-31 16:42:50 +0100

    updated day26 and day29
    Updated day26 as a part of day30

--- Day26_pandas_NATO_Alphabet/NATO-alphabet-start/main.py
+++ Day26_pandas_NATO_Alphabet/NATO-alphabet-start/main.py
@@ -1,10 +1,16 @@
 import pandas
 
-# Create a dictionary from the csv
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for index, row in df.iterrows()}
 
-# Prompt a User to enter a word, create a list of each letter in the word, replace letters, print output
-word = input("Enter a word: ").upper()
-word_in_nato = [nato_dict[letter] for letter in word]
-print(word_in_nato)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        word_in_nato = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Please only enter letters in the alphabet")
+        generate_phonetic()
+    else:
+        print(word_in_nato)
+
+generate_phonetic()
